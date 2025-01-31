425acac27c6fa0a26a7c777bf0d86f039619cce4
WSmadushanka <wijamu.maduu22@gmail.com>
2025-01-31 14:49:22 +0530

    fix commit

--- src/main/java/lk/ijse/nltec/nltecconvertlayerda/view/tdm/tm/CartTm.java
+++ src/main/java/lk/ijse/nltec/nltecconvertlayerda/view/tdm/tm/CartTm.java
@@ -1,4 +1,17 @@
 package lk.ijse.nltec.nltecconvertlayerda.view.tdm.tm;
 
+import com.jfoenix.controls.JFXButton;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CartTm {
+    private String itemName;
+    private int qty;
+    private double unitPrice;
+    private double total;
+    private JFXButton btnRemove;
 }
