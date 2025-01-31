3a55307ff7c14841429ef8b61eba9b6cd9909020
WSmadushanka <wijamu.maduu22@gmail.com>
2025-01-31 14:57:08 +0530

    all bug fix

--- src/main/java/lk/ijse/nltec/nltecconvertlayerda/controller/ItemCardController.java
+++ src/main/java/lk/ijse/nltec/nltecconvertlayerda/controller/ItemCardController.java
@@ -2,8 +2,11 @@ package lk.ijse.nltec.nltecconvertlayerda.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import lk.ijse.nltec.nltecconvertlayerda.entity.ItemCard;
+import lk.ijse.nltec.nltecconvertlayerda.util.MyListener;
 
 public class ItemCardController {
 
@@ -19,4 +22,25 @@ public class ItemCardController {
     @FXML
     private Label lblPrice;
 
+    private ItemCard itemCard;
+    private MyListener myListener;
+
+    public void setData(ItemCard itemCard, MyListener myListener) { // Set Data In Item Card
+        this.itemCard = itemCard;
+        this.myListener = myListener;
+
+        lblItemName.setText(itemCard.getItemName());
+        double profit = itemCard.getPrice() * 0.25;
+        double netPrice = itemCard.getPrice() + profit;
+
+        lblPrice.setText(String.valueOf(netPrice));
+        Image image = new Image(itemCard.getImage(),190, 155, false, true);
+        itemImage.setImage(image);
+
+
+    }
+    @FXML
+    public void click(javafx.scene.input.MouseEvent mouseEvent) {
+        myListener.onClickListener(itemCard);
+    }
 }
--- src/main/java/lk/ijse/nltec/nltecconvertlayerda/dao/custom/impl/ItemSupplierDAOImpl.java
+++ src/main/java/lk/ijse/nltec/nltecconvertlayerda/dao/custom/impl/ItemSupplierDAOImpl.java
@@ -1,4 +1,69 @@
 package lk.ijse.nltec.nltecconvertlayerda.dao.custom.impl;
 
-public class ItemSupplierDAOImpl {
+import lk.ijse.nltec.nltecconvertlayerda.dao.SQLUtill;
+import lk.ijse.nltec.nltecconvertlayerda.dao.custom.ItemSupplierDetailDAO;
+import lk.ijse.nltec.nltecconvertlayerda.entity.ItemSupplierDetail;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ItemSupplierDAOImpl implements ItemSupplierDetailDAO {
+    @Override
+    public ArrayList<ItemSupplierDetail> getAll() throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = SQLUtill.execute("SELECT * FROM item_supplier_detail");//pstm.executeQuery();
+
+        ArrayList<ItemSupplierDetail> itemSupplierList = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String itemId = resultSet.getString(1);
+            String supId = resultSet.getString(2);
+            int handOnQty = resultSet.getInt(3);
+            double unitPrice = resultSet.getDouble(4);
+
+            ItemSupplierDetail itemSupplier = new ItemSupplierDetail(itemId,supId,handOnQty,unitPrice);
+            itemSupplierList.add(itemSupplier);
+        }
+        return itemSupplierList;
+    }
+
+    @Override
+    public boolean save(ItemSupplierDetail is) throws SQLException, ClassNotFoundException {
+        boolean result  = SQLUtill.execute("INSERT INTO item_supplier_detail VALUES(?, ?, ?, ?)",is.getItemId(),is.getSupId(),is.getQty(),is.getUnitPrice());
+        return result;
+    }
+
+
+
+    @Override
+    public boolean update(ItemSupplierDetail is) throws SQLException, ClassNotFoundException {
+        System.out.println("start Item supper detail");
+        boolean result  = SQLUtill.execute("UPDATE item_supplier_detail SET sup_id = ?, qty = ?, unit_price = ? WHERE item_id = ?",is.getSupId(),is.getQty(),is.getUnitPrice(),is.getItemId());
+        return result;
+    }
+
+    @Override
+    public String generateNewID() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public ItemSupplierDetail search(String id) throws SQLException, ClassNotFoundException {
+        ResultSet resultSet = SQLUtill.execute("SELECT * FROM item_supplier_detail WHERE item_id = ?",id);//pstm.executeQuery();
+        if(resultSet.next()) {
+            return new ItemSupplierDetail(
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getInt(3),
+                    resultSet.getDouble(4)
+            );
+        }
+        return null;
+    }
 }
+
