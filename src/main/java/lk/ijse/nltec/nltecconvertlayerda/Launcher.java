5371e1373c434188dc4f9a5fd204357c5d06f32a
WSmadushanka <wijamu.maduu22@gmail.com>
2025-02-01 09:09:51 +0530

    navigate all

--- src/main/java/lk/ijse/nltec/nltecconvertlayerda/Launcher.java
+++ src/main/java/lk/ijse/nltec/nltecconvertlayerda/Launcher.java
@@ -2,6 +2,7 @@ package lk.ijse.nltec.nltecconvertlayerda;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -10,18 +11,18 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class Launcher extends Application {
+    @Override
+    public void start(Stage stage){
+        try{
+            Parent load = FXMLLoader.load((getClass().getResource("/view/login_form.fxml")));
+            Scene scene = new Scene(load);
+            stage.setScene(scene);
+            stage.show();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
-    @Override
-    public void start(Stage stage) throws IOException {
-        AnchorPane rootNode = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/login_form.fxml")));
-
-        Scene scene = new Scene(rootNode);
-        stage.setScene(scene);
-        stage.setTitle("Login form");
-
-        stage.show();
-    }
-
 }
